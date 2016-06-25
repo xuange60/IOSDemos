d87a90d4c3f9729610299b243c5bb2f333cbd79d
zhangxuan <734267682@qq.com>
2016-06-25 19:35:42 +0800

    3

--- GitDemo2/ViewController.m
+++ GitDemo2/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     NSLog(@"My First Demo......");
     NSLog(@"My Second Demo...");
+    NSLog(@"My Second Demo...");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
