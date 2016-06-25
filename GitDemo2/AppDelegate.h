7db15ce6d8fe41dc3dc2bb5e37c09039a18e2d72
zhangxuan <734267682@qq.com>
2016-06-25 18:15:58 +0800

    Initial commit

---
+++ GitDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo2
+//
+//  Created by zhangxuan on 16/6/25.
+//  Copyright © 2016年 zhangxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo2
+//
+//  Created by zhangxuan on 16/6/25.
+//  Copyright © 2016年 zhangxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
